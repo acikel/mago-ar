91e7e75f7514e8fff40e39de9607bde489191134
Aylin Acikel <aylin.acikel@zhdk.ch>
2021-11-09 00:40:35 +0100

    food bug fix

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -13,6 +13,25 @@ public class FoodScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // if left-mouse-button is being held OR there is at least one touch
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)
+        {
+            // get touch position in screen space
+            // (if touch, gets average of all touches)
+            Vector3 screenPos = Input.GetTouch(0).position;
+            // set a distance from the camera
+            screenPos.z = 10.0f;
+            // convert touch position to world space
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+            // get current position of this GameObject
+            Vector3 newPos = transform.position;
+            // set x position to mouse world-space x position
+            newPos.x = worldPos.x;
+            // apply new position
+            transform.position = newPos;
+        }
+
     }
 }
--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -115,7 +115,7 @@ public class GameLogic : MonoBehaviour
     {
         if (mago.GetComponent<Renderer>().isVisible)
         {
-            Vector3 position3D = new Vector3(foodButton.transform.position.x, foodButton.transform.position.y - 4, foodButton.transform.position.z);
+            Vector3 position3D = Camera.main.WorldToScreenPoint(new Vector3(foodButton.transform.position.x, foodButton.transform.position.y - 4, foodButton.transform.position.z));
             Instantiate(foodGameObject, position3D, Quaternion.identity);
         }
     }
