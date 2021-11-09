06c02e173b54dbcd577908bdb34215c9db4764d3
manolo <manuel.wirth@zhdk.ch>
2021-11-09 16:21:43 +0100

    MaGo Prefab mit animations und materials

---
+++ Assets/MagoController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagoController : MonoBehaviour
+{
+    public Animator myAnimatorController;
+
+    public GameObject apple;
+
+    public GameObject brush;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartOrStopRunning();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ReadyForFood();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            EatFood();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            BrushTeeth();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Sleep();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SitDown();
+        }
+
+        
+    }
+
+    public void StartOrStopRunning()
+    {
+        if (apple.activeSelf) apple.SetActive(false);
+        myAnimatorController.SetTrigger("triggerRun");
+    }
+
+    public void ReadyForFood()
+    {
+        if (apple.activeSelf) apple.SetActive(false);
+        myAnimatorController.SetTrigger("triggerReadyForFood");
+    }
+    public void EatFood()
+    {
+        apple.SetActive(true);
+        apple.GetComponent<Animator>().Play("Apple|AppleAction", -1, 0f);
+        myAnimatorController.SetTrigger("triggerEat");
+    }
+
+    public void BrushTeeth()
+    {
+        if (apple.activeSelf) apple.SetActive(false);
+        if (!brush.activeSelf)
+        {
+            brush.SetActive(true);
+            myAnimatorController.SetTrigger("triggerBrushTeeth");
+        }
+        else if (brush.activeSelf)
+        {
+            brush.SetActive(false);
+            myAnimatorController.SetTrigger("triggerBrushTeeth");
+        }
+    }
+
+    public void Sleep()
+    {
+        if (apple.activeSelf) apple.SetActive(false);
+        myAnimatorController.SetTrigger("triggerSleep");
+    }
+
+    public void SitDown()
+    {
+        if (apple.activeSelf) apple.SetActive(false);
+        myAnimatorController.SetTrigger("triggerSit");
+    }
+}
