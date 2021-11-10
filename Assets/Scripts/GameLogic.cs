a85d0e73e615b88cc65fb495e01a11867861deb2
Aylin Acikel <aylin.acikel@zhdk.ch>
2021-11-10 11:10:32 +0100

    Debug

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -142,7 +142,8 @@ public class GameLogic : MonoBehaviour
             
             //Vector3 position3D = Camera.main.WorldToScreenPoint(new Vector3(foodButton.transform.position.x, foodButton.transform.position.y - 4, foodButton.transform.position.z));
             Vector3 position3D = aICamera.WorldToScreenPoint(new Vector3(foodButton.transform.position.x, foodButton.transform.position.y - 4, foodButton.transform.position.z));
-            Debug.Log("seeing mago3");
+            Debug.Log("seeing mago3 touch position: "+ touchPos);
+            Debug.Log("seeing mago3 button position: " + position3D);
             //Instantiate(foodGameObject, position3D, Quaternion.identity);
             Instantiate(foodGameObject, touchPos, Quaternion.identity);
             
