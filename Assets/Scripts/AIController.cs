b0eeb868cf2dc3b0225cf380a7759bdbc5476c85
Aylin Acikel <aylin.acikel@zhdk.ch>
2021-11-11 02:52:18 +0100

    debug and animation transition

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -72,7 +72,7 @@ public class AIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("update called: "+ agent.remainingDistance + " searchNewLocation: " + searchNewLocation + " GameLogic.uiPlaneIsOverlaying: "+ GameLogic.uiPlaneIsOverlaying);
+        Debug.Log("update called: "+ agent.remainingDistance + " searchNewLocation: " + searchNewLocation + " GameLogic.uiPlaneIsOverlaying: "+ GameLogic.uiPlaneIsOverlaying);
         //if (!GameLogic.magoIsBeeingFeeded && agent.remainingDistance < 0.1 && searchNewLocation && !GameLogic.uiPlaneIsOverlaying)
           if (agent.remainingDistance < 0.1 && searchNewLocation && !GameLogic.uiPlaneIsOverlaying)
             {
