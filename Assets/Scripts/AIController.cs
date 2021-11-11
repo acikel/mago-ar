894742ba6c7f5e22eec3224593a79f26c4978459
Aylin Acikel <aylin.acikel@zhdk.ch>
2021-11-11 03:20:41 +0100

    new pos

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -57,7 +57,7 @@ public class AIController : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         //searchNewLocation = false;
         currentPlaneMagoIsPositioned = null;
-        StartCoroutine(WaitBeforeSearchingNewPositionCoroutine(0));
+        StartCoroutine(WaitBeforeSearchingNewPositionCoroutine(10));
         //Debug.Log("Start Method1");
         //StartCoroutine(WaitThenInitializeMago(10));
         //Debug.Log("Start Method2");
