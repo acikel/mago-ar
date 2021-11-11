e6b4d45ca6a9877b764a9e28759c0df6d9302edd
Aylin Acikel <aylin.acikel@zhdk.ch>
2021-11-11 02:54:29 +0100

    check for agent new position

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -155,8 +155,8 @@ public class AIController : MonoBehaviour
         //Get random walkable plane
         //int randomIndex = Random.Range(0, listOfARPlanesWithNavmesh.Count);
         //currentPlaneMagoIsPositioned = listOfARPlanesWithNavmesh[randomIndex];
-        //if (currentPlaneMagoIsPositioned == null)
-        //    return;
+        if (currentPlaneMagoIsPositioned == null)
+            return;
 
         
         nexPositionFound = false;
